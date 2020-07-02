0fd8caa6903e1b89ae446761c5d1a08daeffb992
折原七瀬 <n-orihara@cmind-co.jp>
2020-07-02 15:11:59 +0900

    css情報を追加　15:12

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>tech boost</title>
+
+    <style>
+        h1 {
+            color: red;
+        }
+    </style>
+
+    <link rel="stylesheet" href="index.css">
+
+</head>
+    <body>
+        <div class="box1"> 
+            <h1>Hello world!!!</h1>
+        </div>
+
+        <div class="box2">
+            <p>hello</p>
+        </div>
+
+        <div class="box3">box3</div>
+
+        <button>
+            hello
+        </button>
+
+        <span>
+            hello
+        </span>
+
+        <span>
+            hello
+        </span>
+
+        <span>
+            hello
+        </span>
+    </body>
+</html>
